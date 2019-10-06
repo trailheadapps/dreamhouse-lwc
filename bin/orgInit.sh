1367b929500e868863658417604e83614cf40537
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-10-06 08:25:54 +0200

    Weekly CI builds (#49)

---
+++ bin/orgInit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DURATION=7
+
+if [ "$#" -eq 1 ]; then
+  DURATION=$1
+fi
+
+sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:data:tree:import --plan data/sample-data.json
+sfdx force:org:open -p /lightning/page/home
+echo "Org is set up"
---
+++ bin/releaseUpgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sfdx force:project:upgrade -f
+sfdx muenzpraeger:source:api:set
+sfdx force:org:create -a dreamhouse-release-test -s -f config/project-scratch-def.json -d 7
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:data:tree:import --plan data/sample-data.json
+sfdx force:apex:test:run
+sfdx force:org:open -p /lightning/page/home
+echo "Org is set up"
