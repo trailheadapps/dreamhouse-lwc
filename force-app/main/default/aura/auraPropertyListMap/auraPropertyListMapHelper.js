a4ff41aacfd511f3e9af050d5095924e79c01cd5
Nathan Totten <ntotten@salesforce.com>
2020-05-29 11:33:37 -0400

    Added eslint config for aura

--- force-app/main/default/aura/auraPropertyListMap/auraPropertyListMapHelper.js
+++ force-app/main/default/aura/auraPropertyListMap/auraPropertyListMapHelper.js
@@ -1,6 +1,7 @@
 ({
-    getProperties: function (component) {
+    getProperties: function getProperties(component) {
         var action = component.get('c.getPropertyList');
+
         action.setParams({
             searchKey: component.get('v.searchKey'),
             maxPrice: component.get('v.maxPrice'),
@@ -9,11 +10,14 @@
         });
         action.setCallback(this, function (response) {
             var state = response.getState();
+
             if (state === 'SUCCESS') {
                 var properties = response.getReturnValue();
+
                 component.set('v.properties', properties);
             } else {
                 var toastEvent = $A.get('e.force:showToast');
+
                 toastEvent.setParams({
                     title: 'Error',
                     message: 'Error retrieving properties.',
