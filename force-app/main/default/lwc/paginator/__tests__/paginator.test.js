986a5dcb76d9f5d2f29b23665d3a07a47225dd16
satyasekharcvb <ssekhar@salesforce.com>
2020-07-17 02:01:24 +0530

    Implemented the suggested changes in the review

--- force-app/main/default/lwc/paginator/__tests__/paginator.test.js
+++ force-app/main/default/lwc/paginator/__tests__/paginator.test.js
@@ -9,7 +9,7 @@ describe('c-paginator', () => {
         }
     });
 
-    it('sends "next" and "previous" events on button click', () => {
+    it('sends "next" event on button click', () => {
         // Create initial element
         const element = createElement('c-paginator', {
             is: Paginator
@@ -17,10 +17,9 @@ describe('c-paginator', () => {
         document.body.appendChild(element);
 
         // Mock handlers for child events
-        const handlerPrevious = jest.fn();
         const handlerNext = jest.fn();
+
         // Add event listener to catch child events
-        element.addEventListener('previous', handlerPrevious);
         element.addEventListener('next', handlerNext);
 
         element.shadowRoot
@@ -34,12 +33,39 @@ describe('c-paginator', () => {
         // ending the test and fail the test if the promise rejects.
         return Promise.resolve().then(() => {
             // Validate if mocked events got fired
-            expect(handlerPrevious.mock.calls.length).toBe(1);
             expect(handlerNext.mock.calls.length).toBe(1);
         });
     });
 
-    it('displays total item count, page number, and number of pages', () => {
+    it('sends "previous" event on button click', () => {
+        // Create initial element
+        const element = createElement('c-paginator', {
+            is: Paginator
+        });
+        document.body.appendChild(element);
+
+        // Mock handlers for child events
+        const handlerPrevious = jest.fn();
+
+        // Add event listener to catch child events
+        element.addEventListener('previous', handlerPrevious);
+
+        element.shadowRoot
+            .querySelectorAll('lightning-button-icon')
+            .forEach((button) => {
+                button.click();
+            });
+
+        // Return a promise to wait for any asynchronous DOM updates. Jest
+        // will automatically wait for the Promise chain to complete before
+        // ending the test and fail the test if the promise rejects.
+        return Promise.resolve().then(() => {
+            // Validate if mocked events got fired
+            expect(handlerPrevious.mock.calls.length).toBe(1);
+        });
+    });
+
+    it('displays total item count, page number, and number of pages with zero items', () => {
         // Create initial element
         const element = createElement('c-paginator', {
             is: Paginator
@@ -53,15 +79,29 @@ describe('c-paginator', () => {
 
         // Query div for validating the display message on component init
         const lightningLayoutItemEl = element.shadowRoot.querySelector(
-            'lightning-layout-item.nav-info'
+            '.nav-info'
         );
         //Check for the 0 items message
         expect(lightningLayoutItemEl).not.toBeNull();
         expect(lightningLayoutItemEl.textContent).toBe('0 items • page 0 of 0');
+    });
+
+    it('displays total item count, page number, and number of pages with some items', () => {
+        // Create initial element
+        const element = createElement('c-paginator', {
+            is: Paginator
+        });
+        document.body.appendChild(element);
 
         //Set the public properties for item count greater than zero
         element.pageNumber = 1;
+        element.pageSize = 9;
         element.totalItemCount = 12;
+
+        // Query div for validating the display message on component init
+        const lightningLayoutItemEl = element.shadowRoot.querySelector(
+            '.nav-info'
+        );
 
         // Return a promise to wait for any asynchronous DOM updates. Jest
         // will automatically wait for the Promise chain to complete before
