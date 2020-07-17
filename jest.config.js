46dfca8bded0a5579ad547d6586749ee8ae05140
Alba Rivas <arivas@salesforce.com>
2020-07-17 19:47:53 +0200

    Tests for daysOnMarket (#170)
    * Add tests for daysOnMarket
    
    * Run prettier on json file
    
    * Remove not needed vars
    
    * Updaes
    
    Co-authored-by: pozil <philippe.ozil@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
+        '^@salesforce/schema$': '<rootDir>/force-app/test/jest-mocks/schema',
         '^lightning/navigation$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/navigation'
+            '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
+        '^lightning/messageService$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/messageService'
     }
 };
