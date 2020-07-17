17104d748d7ed874f87fa9381fb9e4253ca1e6a7
satyasekharcvb <31529847+satyasekharcvb@users.noreply.github.com>
2020-07-17 16:39:11 +0530

    Tests for Broker Card (#165)
    * Tests for Broker Card
    
    Co-authored-by: Philippe Ozil <5071767+pozil@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
-    ...jestConfig
+    ...jestConfig,
+    moduleNameMapper: {
+        '^lightning/navigation$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/navigation'
+    }
 };
