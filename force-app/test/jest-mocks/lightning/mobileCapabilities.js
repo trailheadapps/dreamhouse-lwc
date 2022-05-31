4c01c98ac04904da8ffda96de45da9d6ec880d93
Alba Rivas <arivas@salesforce.com>
2022-05-31 13:27:10 +0200

    Add location / distance to property component (#409)

---
+++ force-app/test/jest-mocks/lightning/mobileCapabilities.js
@@ -0,0 +1,21 @@
+// To activate this mocked device location, add to your test:
+//
+// import { setDeviceLocationServiceAvailable } from 'lightning/mobileCapabilities';
+// setDeviceLocationServiceAvailable(true);
+var _available = false;
+
+export const getLocationService = jest.fn().mockImplementation(() => {
+    return {
+        isAvailable: jest.fn().mockReturnValue(_available),
+        getCurrentPosition: jest.fn().mockImplementation(() =>
+            Promise.resolve({
+                latitude: 42.361145,
+                longitude: -71.057083
+            })
+        )
+    };
+});
+
+export const setDeviceLocationServiceAvailable = (value) => {
+    _available = value;
+};
