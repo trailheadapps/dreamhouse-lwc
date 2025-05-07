8be28fe761ffae2416d733d3a107dd4a0c1f075b
Daphne Yang <139700604+daphne-sfdc@users.noreply.github.com>
2025-05-07 05:48:36 -0400

    feat: updates to support using Code Analyzer in place of ESLint extension in Salesforce Extension Pack + fix import in LWC test files to remove error (#714)
    * fix: change the import in LWC test files from 'lwc' to '@lwc/engine-dom' to get rid of errors
    
    * feat: code analyzer extension uses existing .eslintrc.json to lint LWC JS files
    
    * fix: correct setup for eslint-plugin-jest dependency in .eslintrc.json to lint Jest files (LWC tests)
    
    * fix: use existing ESLint recommended rules for linting and Code Analyzer for CI to work

--- force-app/main/default/lwc/propertyListMap/__tests__/propertyListMap.test.js
+++ force-app/main/default/lwc/propertyListMap/__tests__/propertyListMap.test.js
@@ -1,4 +1,4 @@
-import { createElement } from 'lwc';
+import { createElement } from '@lwc/engine-dom';
 import PropertyListMap from 'c/propertyListMap';
 import getPagedPropertyList from '@salesforce/apex/PropertyController.getPagedPropertyList';
 
