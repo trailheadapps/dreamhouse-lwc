d4175824c94d9c11ca0c743f70e0a82978cefe19
Philippe Ozil <pozil@salesforce.com>
2026-04-02 09:43:53 +0200

    fix: reset script deletes custom field

--- reset-demo.sh
+++ reset-demo.sh
@@ -1,5 +1,14 @@
-# Reset all local changes by reverting to git's latest commit
+echo "[1/3] Reset all local changes by reverting to git's latest commit"
 git reset --hard
+echo ""
 
-# Redeploy clean metadata
+echo "[2/3] Remove extra metadata"
+sf project deploy start --manifest "reset-metadata/package.xml" --pre-destructive-changes "reset-metadata/destructiveChangesPre.xml" --purge-on-delete
+echo ""
+
+echo "[3/3] Redeploy clean metadata"
 sf project deploy start --ignore-conflicts -d force-app
+echo ""
+
+echo "Done: demo is reset."
+echo ""
